2891585297007ac0a3c7b628df7a8ba370b81957
kj-lis <kj.lisiak@gmail.com>
2026-02-17 12:39:00 +0100

    Auto-sync 2026-02-17 12:39:00

---
+++ scripts/hpc/8_popLDdecay.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#SBATCH --job-name=zmays_popLDdecay_8
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=5G
+#SBATCH --time=48:00:00
+#SBATCH --account=g102-2488
+#SBATCH --partition=okeanos
+#SBATCH --output=/home/jl430796/MaizePop/logs/zmays_popLDdecay_8.%A_%a.out
+#SBATCH --error=/home/jl430796/MaizePop/logs/zmays_popLDdecay_8.%A_%a.err
+
