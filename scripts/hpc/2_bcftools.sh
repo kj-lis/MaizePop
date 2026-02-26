0eda1aa3974775131fc871e9a61b4149aab8f0c0
kj-lis <kj.lisiak@gmail.com>
2026-02-26 09:10:18 +0100

    Auto-sync 2026-02-26 09:10:18

--- scripts/hpc/2_bcftools.sh
+++ scripts/hpc/2_bcftools.sh
@@ -11,14 +11,14 @@
 #SBATCH --error=/home/jl430796/MaizePop/logs/zmays_bcf_2.%A_%a.err
 #SBATCH --array=1-10
 
-bcftools concat /home/jl430796/MaizePop/data/processed/zea_mays/chr_1_zea_mays_MAF_F.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_mays/chr_2_zea_mays_MAF_F.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_mays/chr_3_zea_mays_MAF_F.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_mays/chr_4_zea_mays_MAF_F.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_mays/chr_5_zea_mays_MAF_F.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_mays/chr_6_zea_mays_MAF_F.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_mays/chr_7_zea_mays_MAF_F.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_mays/chr_8_zea_mays_MAF_F.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_mays/chr_9_zea_mays_MAF_F.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_mays/chr_10_zea_mays_MAF_F.vcf.gz \
--O z -o /home/jl430796/MaizePop/data/processed/zea_mays/chr_zea_mays_all.vcf.gz
+bcftools concat /home/jl430796/MaizePop/data/processed/zea_mays/chr_1_zea_mays_filtr.vcf.gz \
+/home/jl430796/MaizePop/data/processed/zea_mays/chr_2_zea_mays_filtr.vcf.gz \
+/home/jl430796/MaizePop/data/processed/zea_mays/chr_3_zea_mays_filtr.vcf.gz \
+/home/jl430796/MaizePop/data/processed/zea_mays/chr_4_zea_mays_filtr.vcf.gz \
+/home/jl430796/MaizePop/data/processed/zea_mays/chr_5_zea_mays_filtr.vcf.gz \
+/home/jl430796/MaizePop/data/processed/zea_mays/chr_6_zea_mays_filtr.vcf.gz \
+/home/jl430796/MaizePop/data/processed/zea_mays/chr_7_zea_mays_filtr.vcf.gz \
+/home/jl430796/MaizePop/data/processed/zea_mays/chr_8_zea_mays_filtr.vcf.gz \
+/home/jl430796/MaizePop/data/processed/zea_mays/chr_9_zea_mays_filtr.vcf.gz \
+/home/jl430796/MaizePop/data/processed/zea_mays/chr_10_zea_mays_filtr.vcf.gz \
+-O z -o /home/jl430796/MaizePop/data/processed/zea_mays/chr_zea_mays_filtr_all.vcf.gz
