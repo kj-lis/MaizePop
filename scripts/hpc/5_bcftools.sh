76c5161c0cca8a529ad673106d2dcd320af91129
kj-lis <kj.lisiak@gmail.com>
2026-02-26 09:29:44 +0100

    Auto-sync 2026-02-26 09:29:44

--- scripts/hpc/5_bcftools.sh
+++ scripts/hpc/5_bcftools.sh
@@ -17,15 +17,3 @@ bcftools view -S /home/jl430796/MaizePop/metadane/1_IDs.txt /home/marcing/DATA/V
 
 bcftools view /home/jl430796/MaizePop/data/processed/zea_LD/chr_${chr}_zea_LD_1.vcf.gz -i 'F_MISSING<0.2 && MAF>=0.05' -m2 -M2 -v snps \
 -O z -o /home/jl430796/MaizePop/data/processed/zea_LD/chr_${chr}_zea_filtr_LD_1.vcf.gz
-
-bcftools concat /home/jl430796/MaizePop/data/processed/zea_LD/chr_1_zea_filtr_LD_1.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_LD/chr_2_zea_filtr_LD_1.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_LD/chr_3_zea_filtr_LD_1.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_LD/chr_4_zea_filtr_LD_1.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_LD/chr_5_zea_filtr_LD_1.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_LD/chr_6_zea_filtr_LD_1.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_LD/chr_7_zea_filtr_LD_1.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_LD/chr_8_zea_filtr_LD_1.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_LD/chr_9_zea_filtr_LD_1.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_LD/chr_10_zea_filtr_LD_1.vcf.gz \
--O z -o /home/jl430796/MaizePop/data/processed/zea_LD/chr_zea_filtr_LD_1_all.vcf.gz
--- scripts/hpc/6_bcftools.sh
+++ scripts/hpc/6_bcftools.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --job-name=zmmp_bcf_6
+#SBATCH --job-name=zmmp_bcf_6_1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
@@ -10,14 +10,14 @@
 #SBATCH --output=/home/jl430796/MaizePop/logs/zmmp_bcf_6.%A_%a.out
 #SBATCH --error=/home/jl430796/MaizePop/logs/zmmp_bcf_6.%A_%a.err
 
-bcftools concat /home/jl430796/MaizePop/data/processed/zea_mmp/chr_1_zea_mmp_MAF_F.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_mmp/chr_2_zea_mmp_MAF_F.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_mmp/chr_3_zea_mmp_MAF_F.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_mmp/chr_4_zea_mmp_MAF_F.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_mmp/chr_5_zea_mmp_MAF_F.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_mmp/chr_6_zea_mmp_MAF_F.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_mmp/chr_7_zea_mmp_MAF_F.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_mmp/chr_8_zea_mmp_MAF_F.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_mmp/chr_9_zea_mmp_MAF_F.vcf.gz \
-/home/jl430796/MaizePop/data/processed/zea_mmp/chr_10_zea_mmp_MAF_F.vcf.gz \
--O z -o /home/jl430796/MaizePop/data/processed/zea_mmp/chr_zea_mmp_all.vcf.gz
+bcftools concat /home/jl430796/MaizePop/data/processed/zea_LD/chr_1_zea_filtr_LD_1.vcf.gz \
+/home/jl430796/MaizePop/data/processed/zea_LD/chr_2_zea_filtr_LD_1.vcf.gz \
+/home/jl430796/MaizePop/data/processed/zea_LD/chr_3_zea_filtr_LD_1.vcf.gz \
+/home/jl430796/MaizePop/data/processed/zea_LD/chr_4_zea_filtr_LD_1.vcf.gz \
+/home/jl430796/MaizePop/data/processed/zea_LD/chr_5_zea_filtr_LD_1.vcf.gz \
+/home/jl430796/MaizePop/data/processed/zea_LD/chr_6_zea_filtr_LD_1.vcf.gz \
+/home/jl430796/MaizePop/data/processed/zea_LD/chr_7_zea_filtr_LD_1.vcf.gz \
+/home/jl430796/MaizePop/data/processed/zea_LD/chr_8_zea_filtr_LD_1.vcf.gz \
+/home/jl430796/MaizePop/data/processed/zea_LD/chr_9_zea_filtr_LD_1.vcf.gz \
+/home/jl430796/MaizePop/data/processed/zea_LD/chr_10_zea_filtr_LD_1.vcf.gz \
+-O z -o /home/jl430796/MaizePop/data/processed/zea_LD/chr_zea_filtr_LD_1_all.vcf.gz
