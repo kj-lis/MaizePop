2f2547cf8273759ff89172b027a54725b7508f88
kj-lis <kj.lisiak@gmail.com>
2026-02-27 15:16:42 +0100

    Auto-sync 2026-02-27 15:16:42

--- scripts/hpc/6_popLDdecay.sh
+++ scripts/hpc/6_popLDdecay.sh
@@ -10,8 +10,17 @@
 #SBATCH --output=/home/jl430796/MaizePop/logs/zmays_LD_6.%A_%a.out
 #SBATCH --error=/home/jl430796/MaizePop/logs/zmays_LD_6.%A_%a.err
 
-PopLDdecay -InVCF /home/jl430796/MaizePop/data/processed/zea_LD/chr_zea_mays_LD1.vcf.gz -OutStat zea_1_LD -MaxDist 500
-PopLDdecay -InVCF /home/jl430796/MaizePop/data/processed/zea_LD/chr_zea_mays_LD1.vcf.gz -OutStat zea_2_LD -MaxDist 500
-PopLDdecay -InVCF /home/jl430796/MaizePop/data/processed/zea_LD/chr_zea_mays_LD1.vcf.gz -OutStat zea_3_LD -MaxDist 500
-PopLDdecay -InVCF /home/jl430796/MaizePop/data/processed/zea_LD/chr_zea_mays_LD1.vcf.gz -OutStat zea_4_LD -MaxDist 500
-PopLDdecay -InVCF /home/jl430796/MaizePop/data/processed/zea_LD/chr_zea_mays_LD1.vcf.gz -OutStat group1 -MaxDist 500
+PopLDdecay -InVCF /home/jl430796/MaizePop/data/processed/zea_LD/chr_zea_mays_LD1.vcf.gz 
+-OutStat /home/jl430796/MaizePop/results/full/zea_LD/zea_1_LD -MaxDist 500
+
+PopLDdecay -InVCF /home/jl430796/MaizePop/data/processed/zea_LD/chr_zea_mays_LD2.vcf.gz 
+-OutStat /home/jl430796/MaizePop/results/full/zea_LD/zea_1_LD -MaxDist 500
+
+PopLDdecay -InVCF /home/jl430796/MaizePop/data/processed/zea_LD/chr_zea_mays_LD3.vcf.gz 
+-OutStat home/jl430796/MaizePop/data/processed/zea_LD/zea_3_LD -MaxDist 500
+
+PopLDdecay -InVCF /home/jl430796/MaizePop/data/processed/zea_LD/chr_zea_mays_LD4.vcf.gz 
+-OutStat home/jl430796/MaizePop/data/processed/zea_LD/zea_4_LD -MaxDist 500
+
+PopLDdecay -InVCF /home/jl430796/MaizePop/data/processed/zea_LD/chr_zea_mays_LD5.vcf.gz 
+-OutStat home/jl430796/MaizePop/data/processed/zea_LD/group1 -MaxDist 500
