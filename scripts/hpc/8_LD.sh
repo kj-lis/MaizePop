0d3ef1a610f52822deb96799b3bdf25bb7218620
kj-lis <kj.lisiak@gmail.com>
2026-03-01 21:19:51 +0100

    Auto-sync 2026-03-01 21:19:51

---
+++ scripts/hpc/8_LD.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#SBATCH --job-name=zmays_bcf_5
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=5G
+#SBATCH --time=48:00:00
+#SBATCH --account=g102-2488
+#SBATCH --partition=okeanos
+#SBATCH --output=/home/jl430796/MaizePop/logs/zmays_bcf_5.%A_%a.out
+#SBATCH --error=/home/jl430796/MaizePop/logs/zmays_bcf_5.%A_%a.err
+#SBATCH --array=1-5
+
+LD=${SLURM_ARRAY_TASK_ID}
+
+bcftools view -S /home/jl430796/MaizePop/metadane/${LD}_IDs.txt /home/jl430796/MaizePop/data/processed/zea_mays/chr_zea_mays_all_filtr.vcf.gz \
+-O z -o /home/jl430796/MaizePop/data/processed/zea_LD/chr_zea_mays_LD${LD}.vcf.gz
---
+++ scripts/hpc/9_LD.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#SBATCH --job-name=zmays_LD_6
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=5G
+#SBATCH --time=48:00:00
+#SBATCH --account=g102-2488
+#SBATCH --partition=okeanos
+#SBATCH --output=/home/jl430796/MaizePop/logs/zmays_LD_6.%A_%a.out
+#SBATCH --error=/home/jl430796/MaizePop/logs/zmays_LD_6.%A_%a.err
+#SBATCH --array=1-5
+
+LD=${SLURM_ARRAY_TASK_ID}
+
+/home/jl430796/PopLDdecay/bin/PopLDdecay \
+-InVCF /home/jl430796/MaizePop/data/processed/zea_LD/chr_zea_mays_LD${LD}.vcf.gz \
+-OutStat /home/jl430796/MaizePop/results/full/zea_LD/zea_${LD}_LD -MaxDist 500 -MAF 0.05
