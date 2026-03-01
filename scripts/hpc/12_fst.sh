bc3a2aae0a63b1adf51dadd2df3264fca19fcbce
kj-lis <kj.lisiak@gmail.com>
2026-03-01 22:22:26 +0100

    Auto-sync 2026-03-01 22:22:26

--- scripts/hpc/12_fst.sh
+++ scripts/hpc/12_fst.sh
@@ -10,7 +10,7 @@
 #SBATCH --output=/home/jl430796/MaizePop/logs/zea_fst_12.%A_%a.out
 #SBATCH --error=/home/jl430796/MaizePop/logs/zea_fst_12.%A_%a.err
 
-gcta64 --bfile /home/jl430796/MaizePop/data/processed/zea_fst/chr_zea_fst_all_plink_3 \
+gcta64 --bfile /home/jl430796/MaizePop/data/processed/zea_fst/chr_zea_fst_chr1 \
        --fst \
        --sub-popu /home/jl430796/MaizePop/metadane/zea_fst_metadane_sorted.txt \
        --out /home/jl430796/MaizePop/results/full/zea_fst/zea_mays_fst
