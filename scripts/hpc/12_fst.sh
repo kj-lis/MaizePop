b79b1221630f155a856511b07563ca06076c52bb
kj-lis <kj.lisiak@gmail.com>
2026-03-01 22:06:11 +0100

    Auto-sync 2026-03-01 22:06:11

--- scripts/hpc/12_fst.sh
+++ scripts/hpc/12_fst.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=16G
+#SBATCH --mem=5G
 #SBATCH --time=48:00:00
 #SBATCH --account=g102-2488
 #SBATCH --partition=okeanos
@@ -12,5 +12,5 @@
 
 gcta64 --bfile /home/jl430796/MaizePop/data/processed/zea_fst/chr_zea_fst_all_plink_3 \
        --fst \
-       --sub-popu /home/jl430796/MaizePop/metadane/zea_fst_metadane.txt \
+       --sub-popu /home/jl430796/MaizePop/metadane/zea_fst_metadane_sorted.txt \
        --out /home/jl430796/MaizePop/results/full/zea_fst/zea_mays_fst
