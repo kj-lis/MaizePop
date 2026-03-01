79c30f9749edbf9207f99210169a25e0ded326df
kj-lis <kj.lisiak@gmail.com>
2026-03-01 21:52:51 +0100

    Auto-sync 2026-03-01 21:52:51

--- scripts/hpc/12_fst.sh
+++ scripts/hpc/12_fst.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --job-name=zmays_gcta_8
+#SBATCH --job-name=zea_fst_12
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
@@ -7,8 +7,8 @@
 #SBATCH --time=48:00:00
 #SBATCH --account=g102-2488
 #SBATCH --partition=okeanos
-#SBATCH --output=/home/jl430796/MaizePop/logs/zmays_gcta_8.%A_%a.out
-#SBATCH --error=/home/jl430796/MaizePop/logs/zmays_gcta_8.%A_%a.err
+#SBATCH --output=/home/jl430796/MaizePop/logs/zea_fst_12.%A_%a.out
+#SBATCH --error=/home/jl430796/MaizePop/logs/zea_fst_12.%A_%a.err
 
 gcta64 --bfile /home/jl430796/MaizePop/data/processed/zea_fst/chr_zea_fst_all_plink_3 \
        --fst \
