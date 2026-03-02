e3c5683f363ecaa384c1829b2586972b28946271
kj-lis <kj.lisiak@gmail.com>
2026-03-02 14:16:09 +0100

    Auto-sync 2026-03-02 14:16:09

--- scripts/R/10_LD.R
+++ scripts/R/10_LD.R
@@ -1,39 +1,24 @@
-LD1 <- read.table("/home/kuba/Desktop/full/zea_LD/zea_1_LD.stat.gz", header=TRUE)
-LD2 <- read.table("/home/kuba/Desktop/full/zea_LD/zea_2_LD.stat.gz", header=TRUE)
-LD3 <- read.table("/home/kuba/Desktop/full/zea_LD/zea_3_LD.stat.gz", header=TRUE)
-LD4 <- read.table("/home/kuba/Desktop/full/zea_LD/zea_4_LD.stat.gz", header=TRUE)
-LD5 <- read.table("/home/kuba/Desktop/full/zea_LD/zea_5_LD.stat.gz", header=TRUE)
-
-plot(LD1$Dist, LD1$Mean_r2, type="l")
-plot(LD2$Dist, LD2$Mean_r2, type="l")
-plot(LD3$Dist, LD3$Mean_r2, type="l")
-plot(LD4$Dist, LD4$Mean_r2, type="l")
-plot(LD5$Dist, LD5$Mean_r2, type="l")
-
-
-###############################################
-
-
 library(ggplot2)
 library(dplyr)
 library(readr)
 
-ld_folder <- "..."
+ld_folder <- "/home/kuba/Desktop/full/zea_LD"
 files <- list.files(ld_folder, pattern="\\.stat\\.gz$", full.names = TRUE)
 
 groups <- sub("\\.stat\\.gz$", "", basename(files))
 
 ld_list <- lapply(seq_along(files), function(i) {
-  df <- read.table(files[i], header=TRUE)
+  df <- read.table(files[i], header = FALSE)
+  df <- df[, c("V4", "V2")]
+  colnames(df) <- c("Dist", "Mean_r2")
   df$Group <- groups[i]
   return(df)
 })
 
 ld_data <- dplyr::bind_rows(ld_list)
 
-
 ggplot(ld_data, aes(x=Dist, y=Mean_r2, color=Group)) +
-  geom_line(size=1.2) +                 
+  geom_line(linewidth=1.2) +                 
   scale_color_brewer(palette="Set1") +  
   labs(
     x="Distance between SNPs (kb)",
