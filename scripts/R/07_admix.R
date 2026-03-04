1f0ed66fe8f955ef40bf0c0c3a27559e82b41cc1
kj-lis <kj.lisiak@gmail.com>
2026-03-04 16:20:19 +0100

    Auto-sync 2026-03-04 16:20:19

--- scripts/R/07_admix.R
+++ scripts/R/07_admix.R
@@ -2,14 +2,11 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
-# jeśli jeszcze nie masz maxQ
 merged$maxQ <- apply(merged[,paste0("K",1:5)], 1, max)
 
-# sortowanie: najpierw klaster, potem czystość malejąco
 merged_sorted <- merged %>%
   arrange(Assigned_cluster, desc(maxQ))
 
-# nadajemy kolejność na osi X
 merged_sorted$Order <- 1:nrow(merged_sorted)
 
 adm_long <- merged_sorted %>%
@@ -17,38 +14,94 @@ adm_long <- merged_sorted %>%
                names_to = "Cluster",
                values_to = "Ancestry")
 
-boundaries <- cumsum(table(merged_sorted$Assigned_cluster))
 
-ggplot(adm_long, aes(x = Order, y = Ancestry, fill = Cluster)) +
+boundaries <- cumsum(table(merged_sorted$Assigned_cluster))
+boundaries <- boundaries[-length(boundaries)]
+
+cluster_labels <- c(
+  "K1 - Iodent (Poland)",
+  "K2 - SS",
+  "K3 - NSS, Tropical",
+  "K4 - NSS (Poland)",
+  "K5 - Zea mays subsp."
+)
+
+legend_title_text <- "Subpopulations"
+
+legend_text_size  <- 12
+legend_title_size <- 14
+axis_text_size    <- 12
+axis_title_size   <- 14
+
+x_title_margin <- 15
+y_title_margin <- 15
+
+cluster_line_offset <- 0.00
+
+# usuń słupki z Ancestry == 0
+adm_long_nonzero <- adm_long %>%
+  filter(Ancestry > 0)
+
+# rysowanie wykresu
+ggplot(adm_long_nonzero, aes(x = Order, y = Ancestry, fill = Cluster)) +
+  
   geom_bar(stat = "identity", width = 1) +
   
   geom_segment(
     data = data.frame(x = boundaries),
-    aes(x = x, xend = x, y = 0.00, yend = 1),  # <-- tu kontrolujesz odstęp
+    aes(x = x + 0.5,
+        xend = x + 0.5,
+        y = cluster_line_offset,
+        yend = 1),
     inherit.aes = FALSE,
     color = "black",
     linewidth = 0.8
   ) +
   
-  scale_fill_manual(values = c(
-    "#1b9e77",
-    "#d95f02",
-    "#7570b3",
-    "#e7298a",
-    "#66a61e"
-  )) +
+  scale_fill_manual(
+    values = c(
+      "#00BF7D",
+      "#00B0F6",
+      "#E76BF3",
+      "#F8766D",
+      "#f09a4a"
+    ),
+    labels = cluster_labels,
+    name   = legend_title_text
+  ) +
+  
+  # ustawienie przedziałów osi Y
+  scale_y_continuous(breaks = seq(0, 1, 0.2)) +
   
   coord_cartesian(ylim = c(0, 1)) +
   
   theme_classic() +
   theme(
-    axis.text.x = element_blank(),
+    # usuń ticki i tekst na osi X, zostaw podpis
+    axis.text.x  = element_blank(),
     axis.ticks.x = element_blank(),
-    panel.grid = element_blank(),
-    legend.title = element_blank()
+    axis.text.y  = element_text(size = axis_text_size),
+    
+    axis.title.x = element_text(
+      size = axis_title_size,
+      margin = margin(t = x_title_margin)
+    ),
+    
+    axis.title.y = element_text(
+      size = axis_title_size,
+      margin = margin(r = y_title_margin)
+    ),
+    
+    legend.title = element_text(size = legend_title_size, face = "bold"),
+    legend.text  = element_text(size = legend_text_size),
+    
+    panel.grid = element_blank()
   ) +
-  labs(x = "Lines",
-       y = "Ancestry")
+  
+  labs(
+    x = "Lines",
+    y = "Ancestry"
+  )
 
 
 ################################################
