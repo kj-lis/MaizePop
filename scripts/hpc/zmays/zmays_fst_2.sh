43bab470bea0bfaf716c882ce000b928997ec204
kj-lis <kj.lisiak@gmail.com>
2026-03-06 15:38:57 +0100

    Auto-sync 2026-03-06 15:38:57

--- scripts/hpc/zmays/zmays_fst_2.sh
+++ scripts/hpc/zmays/zmays_fst_2.sh
@@ -3,14 +3,17 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=16G
+#SBATCH --mem=5G
 #SBATCH --time=48:00:00
 #SBATCH --account=g102-2488
 #SBATCH --partition=okeanos
 #SBATCH --output=/home/jl430796/MaizePop/logs/zea_fst_12.%A_%a.out
 #SBATCH --error=/home/jl430796/MaizePop/logs/zea_fst_12.%A_%a.err
 
-/home/jl430796/gcta-1.95.1-linux-x86_64/gcta64 --bfile /home/jl430796/MaizePop/data/processed/zea_fst/chr_zea_fst_all_plink_3 \
-       --fst \
-       --sub-popu /home/jl430796/MaizePop/metadane/fst/zea_fst_metadane_sorted.txt \
-       --out /home/jl430796/MaizePop/results/full/zea_fst/zea_fst
+#do przerobienia
+
+/home/jl430796/software/gcta-1.95.1-linux-x86_64/gcta64 \
+        --bfile /home/jl430796/MaizePop/data/processed/zea_fst/chr_zea_fst_all_plink_3 \
+        --fst \
+        --sub-popu /home/jl430796/MaizePop/metadane/fst/zea_fst_metadane_sorted.txt \
+        --out /home/jl430796/MaizePop/results/full/zea_fst/zea_fst
