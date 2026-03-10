fb7ed0e013414404e236cf34c304f3c59b92bf18
kj-lis <kj.lisiak@gmail.com>
2026-03-10 11:26:03 +0100

    Auto-sync 2026-03-10 11:26:03

--- scripts/hpc/zmays/05_zmays_admix.sh
+++ scripts/hpc/zmays/05_zmays_admix.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --job-name=zmays_admix_4
+#SBATCH --job-name=zmays_admix_5
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
@@ -7,8 +7,8 @@
 #SBATCH --time=48:00:00
 #SBATCH --account=g102-2488
 #SBATCH --partition=okeanos
-#SBATCH --output=/home/jl430796/MaizePop/logs/zmays_admix_4.%A_%a.out
-#SBATCH --error=/home/jl430796/MaizePop/logs/zmays_admix_4.%A_%a.err
+#SBATCH --output=/home/jl430796/MaizePop/logs/zmays_admix_5.%A_%a.out
+#SBATCH --error=/home/jl430796/MaizePop/logs/zmays_admix_5.%A_%a.err
 
 for K in {2..10}; do
     admixture /home/jl430796/MaizePop/data/processed/zmays/PCA/chr_all_zmays_plink_3.bed $K
