826227cb3277785d14f413c568b34523e2cd9c2a
kj-lis <kj.lisiak@gmail.com>
2026-03-12 22:15:56 +0100

    Auto-sync 2026-03-12 22:15:56

--- scripts/hpc/12_LDdecay_wykres_1.sh
+++ scripts/hpc/12_LDdecay_wykres_1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --job-name=zmays_13
+#SBATCH --job-name=zmays_12
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
@@ -7,14 +7,14 @@
 #SBATCH --time=48:00:00
 #SBATCH --account=g102-2488
 #SBATCH --partition=okeanos
-#SBATCH --output=/home/jl430796/MaizePop/logs/zmays_11.%A_%a.out
-#SBATCH --error=/home/jl430796/MaizePop/logs/zmays_11.%A_%a.err
+#SBATCH --output=/home/jl430796/MaizePop/logs/zmays_12.%A_%a.out
+#SBATCH --error=/home/jl430796/MaizePop/logs/zmays_12.%A_%a.err
 #SBATCH --array=1-7
 
-Pop=${SLURM_ARRAY_TASK_ID}
+LD=${SLURM_ARRAY_TASK_ID}
 
 perl      /home/jl430796/software/PopLDdecay/bin/Plot_OnePop.pl \
--inFile   /home/jl430796/MaizePop/results/full/zea_LD/zea_LD${Pop}.stat.gz \
--output   /home/jl430796/MaizePop/results/full/zea_LD/zea_LD${Pop}
+-inFile   /home/jl430796/MaizePop/data/processed/03_LDdecay/zea_LD${LD}.stat.gz \
+-output   /home/jl430796/MaizePop/results/full/zea_LD${Pop}
 
 perl  bin/Plot_MutiPop.pl  -inList  Pop.ResultPath.list  -output Fig
---
+++ scripts/hpc/15_fixation_index.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#SBATCH --job-name=zmays_15
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=5G
+#SBATCH --time=48:00:00
+#SBATCH --account=g102-2488
+#SBATCH --partition=okeanos
+#SBATCH --output=/home/jl430796/MaizePop/logs/zmays_15.%A_%a.out
+#SBATCH --error=/home/jl430796/MaizePop/logs/zmays_15.%A_%a.err
+
+/home/jl430796/software/gcta-1.95.1-linux-x86_64/gcta64 \
+        --bfile /home/jl430796/MaizePop/results/full/04_fixation_index/zea_Parviglumis_Tropical \
+        --fst \
+        --sub-popu /home/jl430796/MaizePop/metadane/05_fixation_index/Parviglumis_Tropical_fst_IDs.txt \
+        --out /home/jl430796/MaizePop/data/processed/04_fixation_index/...
