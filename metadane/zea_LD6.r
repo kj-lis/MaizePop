9af1c14edca1d0c42fbbbdddbef04da6555c7d49
kj-lis <kj.lisiak@gmail.com>
2026-03-12 22:22:19 +0100

    Auto-sync 2026-03-12 22:22:19

--- metadane/zea_LD6.r
+++
@@ -1,8 +0,0 @@
-read.table("zea_LD6.bin")->data
-pdf("zea_LD6.pdf")
-plot(data[,1]/1000,data[,2],type="l",col="blue",main="LD decay",xlab="Distance(Kb)",xlim=c(0,1000),ylab=expression(r^{2}),bty="n",lwd=2)
-dev.off()
-png("zea_LD6.png")
-plot(data[,1]/1000,data[,2],type="l",col="blue",main="LD decay",xlab="Distance(Kb)",xlim=c(0,1000),ylab=expression(r^{2}),bty="n",lwd=2)
-dev.off()
-
