1c58c96dbe3a6e0aad4234c98430132cd6cd7f8f
kj-lis <kj.lisiak@gmail.com>
2026-03-13 14:18:55 +0100

    Auto-sync 2026-03-13 14:18:55

--- scripts/hpc/12_LDdecay.sh
+++ scripts/hpc/12_LDdecay.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --job-name=zmays_11
+#SBATCH --job-name=zmays_12
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
@@ -7,8 +7,8 @@
 #SBATCH --time=48:00:00
 #SBATCH --account=g102-2488
 #SBATCH --partition=okeanos
-#SBATCH --output=/home/jl430796/MaizePop/logs/zmays_11.%A_%a.out
-#SBATCH --error=/home/jl430796/MaizePop/logs/zmays_11.%A_%a.err
+#SBATCH --output=/home/jl430796/MaizePop/logs/zmays_12.%A_%a.out
+#SBATCH --error=/home/jl430796/MaizePop/logs/zmays_12.%A_%a.err
 #SBATCH --array=1-7
 
 LD=${SLURM_ARRAY_TASK_ID}
--- scripts/hpc/15_fixation_index.sh
+++ scripts/hpc/15_fixation_index.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --job-name=zmays_14
+#SBATCH --job-name=zmays_15
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
@@ -7,8 +7,8 @@
 #SBATCH --time=48:00:00
 #SBATCH --account=g102-2488
 #SBATCH --partition=okeanos
-#SBATCH --output=/home/jl430796/MaizePop/logs/zmays_14.%A_%a.out
-#SBATCH --error=/home/jl430796/MaizePop/logs/zmays_14.%A_%a.err
+#SBATCH --output=/home/jl430796/MaizePop/logs/zmays_15.%A_%a.out
+#SBATCH --error=/home/jl430796/MaizePop/logs/zmays_15.%A_%a.err
 
 /home/jl430796/software/gcta-1.95.1-linux-x86_64/gcta64 \
         --bfile /home/jl430796/MaizePop/data/processed/04_fixation_index/zea_Parviglumis_Tropical \
