06155da22c953e947be140b1d3564a2951ae3ec0
kj-lis <kj.lisiak@gmail.com>
2026-03-19 15:09:19 +0100

    Auto-sync 2026-03-19 15:09:19

--- scripts/RStudio/16_fixation_index_analiza.R
+++ scripts/RStudio/16_fixation_index_analiza.R
@@ -11,30 +11,41 @@ summary(TNS_fst$FST)
 PT_fst_2 <- PT_fst[PT_fst$Fst >= 0, ]
 TI_fst_2 <- TI_fst[TI_fst$Fst >= 0, ]
 TSS_fst_2 <- TSS_fst[TSS_fst$Fst >= 0, ]
-TNS_fst_2 <- TNS_fst[TNS_fst$FST >= 0, ]
+TNS_fst_2 <- TNS_fst[TNS_fst$Fst >= 0, ]
 
-PT_fst_clean <- PT_fst_2[is.na(PT_fst_2$Fst), ]
-TI_fst_clean <- TI_fst_2[is.na(TI_fst_2$Fst), ]
-TSS_fst_clean <- TSS_fst_2[is.na(TSS_fst_2$Fst), ]
-TNS_fst_clean <- TNS_fst_2[is.na(TNS_fst_2$Fst), ]
+PT_fst_clean <- PT_fst_2[!is.na(PT_fst_2$Fst), ]
+TI_fst_clean <- TI_fst_2[!is.na(TI_fst_2$Fst), ]
+TSS_fst_clean <- TSS_fst_2[!is.na(TSS_fst_2$Fst), ]
+TNS_fst_clean <- TNS_fst_2[!is.na(TNS_fst_2$Fst), ]
 
 library(ggplot2)
 
 ggplot(PT_fst_clean, aes(x = Fst)) +
-  geom_histogram(bins = 100, fill = "steelblue", color = "black") +
-  theme_minimal(panel.grid = element_blank()) +
+  geom_histogram(bins = 50, fill = "steelblue", color = "black") +
+  theme_minimal() + 
+  theme(panel.grid = element_blank()) +
   labs(
     x = "fixation index",
     y = "number of SNPs")
 
+PT_fst_chr1 <- PT_fst_clean[PT_fst_clean$Chr == 1, ]
+PT_fst_chr2 <- PT_fst_clean[PT_fst_clean$Chr == 2, ]
+PT_fst_chr3 <- PT_fst_clean[PT_fst_clean$Chr == 3, ]
+PT_fst_chr4 <- PT_fst_clean[PT_fst_clean$Chr == 4, ]
+PT_fst_chr5 <- PT_fst_clean[PT_fst_clean$Chr == 5, ]
+PT_fst_chr6 <- PT_fst_clean[PT_fst_clean$Chr == 6, ]
+PT_fst_chr7 <- PT_fst_clean[PT_fst_clean$Chr == 7, ]
+PT_fst_chr8 <- PT_fst_clean[PT_fst_clean$Chr == 8, ]
+PT_fst_chr9 <- PT_fst_clean[PT_fst_clean$Chr == 9, ]
+PT_fst_chr10 <- PT_fst_clean[PT_fst_clean$Chr == 10, ]
+
+library(GenWin)
+
+PT_fst_chr10_Spline <- splineAnalyze(Y=PT_fst_chr10$Fst,map=PT_fst_chr10$bp,smoothness=100,
+                            plotRaw=TRUE,plotWindows=TRUE,method=4)
 
 
-
-
-
-
-
-
+################################
 
 
 
@@ -68,18 +79,19 @@ points(mean_fst, 1, col = "red", pch = 19)
 
 
 library(dplyr)
-fst_by_chr <- fst_data %>%
-  group_by(CHR) %>%
-  summarise(mean_FST = mean(FST, na.rm=TRUE))
+fst_by_chr <- TI_fst %>%
+  group_by(Chr) %>%
+  summarise(mean_fst = mean(Fst, na.rm=TRUE))
 
-barplot(fst_by_chr$mean_FST,
-        names.arg = fst_by_chr$CHR,
+barplot(fst_by_chr$mean_fst,
+        names.arg = fst_by_chr$Chr,
         col = "steelblue",
         xlab = "Chromosome",
         ylab = "Mean FST",
         main = "Mean FST per chromosome")
+mean_fst <- mean(TI_fst_clean$Fst, na.rm = TRUE)
 
-plot(density(fst_data$FST, na.rm=TRUE),
+plot(density(TI_fst_clean$Fst, na.rm=TRUE),
      main="Density of FST",
      xlab="FST",
      ylab="Density",
@@ -89,15 +101,6 @@ abline(v = mean_fst, col="red", lwd=2)
 
 
 
-
-
-
-
-
-
-
-
-################################
 
 
 fst1<- read.table("/home/kuba/Desktop/Parviglumis_Tropical.fst", header=TRUE)
