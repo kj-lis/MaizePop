4f1df94f858a33e6a58d25547fc8e1293bf31aa2
kj-lis <kj.lisiak@gmail.com>
2026-03-19 13:43:34 +0100

    Auto-sync 2026-03-19 13:43:34

--- scripts/hpc/15_fixation_index.sh
+++ scripts/hpc/15_fixation_index.sh
@@ -1,4 +1,4 @@
-/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./Parviglumis_Tropical --fst --sub-popu ./Parviglumis_Tropical_fst_IDs.txt  --out ./Parviglumis_Tropical
-/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./Tropical_Iodent --fst --sub-popu ./Tropical_Iodent_fst_IDs.txt  --out ./Tropical_Iodent
-/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./Tropical_SS --fst --sub-popu ./Tropical_SS_fst_IDs.txt  --out ./Tropical_SS
-/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./Tropical_NSS --fst --sub-popu ./Tropical_NSS_fst_IDs.txt  --out ./Tropical_NSS
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./04_fixation_index/Parviglumis_Tropical --fst --sub-popu ./Parviglumis_Tropical_fst_IDs.txt  --out ./Parviglumis_Tropical
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./04_fixation_index/Tropical_Iodent --fst --sub-popu ./Tropical_Iodent_fst_IDs.txt  --out ./Tropical_Iodent
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./04_fixation_index/Tropical_SS --fst --sub-popu ./Tropical_SS_fst_IDs.txt  --out ./Tropical_SS
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./04_fixation_index/Tropical_NSS --fst --sub-popu ./Tropical_NSS_fst_IDs.txt  --out ./Tropical_NSS
