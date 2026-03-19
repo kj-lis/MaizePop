770ae7953fa4c0ea677cdd4bf59cdd1fe45f4ec6
kj-lis <kj.lisiak@gmail.com>
2026-03-19 11:39:41 +0100

    Auto-sync 2026-03-19 11:39:41

--- scripts/hpc/15_fixation_index.sh
+++ scripts/hpc/15_fixation_index.sh
@@ -1,1 +1,4 @@
-C:/Users/kjlis/Desktop/gcta-1.95.1-windows-x86_64/gcta-1.95.1-windows-x86_64/gcta64 --bfile zea_Parviglumis_Tropical --fst --sub-popu Parviglumis_Tropical_fst_IDs.txt  --out Parviglumis_Tropical_fst
+home/kuba/Desktop/ --bfile ./Parviglumis_Tropical --fst --sub-popu ./Parviglumis_Tropical_fst_IDs.txt  --out ./Parviglumis_Tropical
+home/kuba/Desktop/ --bfile ./Tropical_Iodent --fst --sub-popu ./Tropical_Iodent_fst_IDs.txt  --out ./Tropical_Iodent
+home/kuba/Desktop/ --bfile ./Tropical_SS --fst --sub-popu ./Tropical_SS_fst_IDs.txt  --out ./Tropical_SS
+home/kuba/Desktop/ --bfile ./Tropical_NSS --fst --sub-popu ./Tropical_NSS_fst_IDs.txt  --out ./Tropical_NSS
