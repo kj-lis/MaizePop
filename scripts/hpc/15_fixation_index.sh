00acc50a9eda46a6edcc95b4026ddc21818ceed6
kj-lis <kj.lisiak@gmail.com>
2026-03-27 15:27:40 +0100

    Auto-sync 2026-03-27 15:27:40

--- scripts/hpc/15_fixation_index.sh
+++ scripts/hpc/15_fixation_index.sh
@@ -1,4 +1,21 @@
 /home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./04_fixation_index/Parviglumis_Tropical --fst --sub-popu ./Parviglumis_Tropical_fst_IDs.txt  --out ./Parviglumis_Tropical
+
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./04_fixation_index/Tropical_Iodent_era1 --fst --sub-popu ./Tropical_Iodent_era1_fst_IDs.txt  --out ./Tropical_Iodent_era1
+
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./04_fixation_index/Tropical_SS_era1 --fst --sub-popu ./Tropical_SS_era1_fst_IDs.txt  --out ./Tropical_SS_era1
+
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./04_fixation_index/Iodent_era1_era2 --fst --sub-popu ./Iodent_era1_era2_fst_IDs.txt  --out ./Iodent_era1_era2
+
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./04_fixation_index/SS_era1_era2 --fst --sub-popu ./SS_era1_era2_fst_IDs.txt  --out ./SS_era1_era2
+
+
+################################
+
+
 /home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./04_fixation_index/Tropical_Iodent --fst --sub-popu ./Tropical_Iodent_fst_IDs.txt  --out ./Tropical_Iodent
+
 /home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./04_fixation_index/Tropical_SS --fst --sub-popu ./Tropical_SS_fst_IDs.txt  --out ./Tropical_SS
+
 /home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./04_fixation_index/Tropical_NSS --fst --sub-popu ./Tropical_NSS_fst_IDs.txt  --out ./Tropical_NSS
+
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./04_fixation_index/SS_NSS --fst --sub-popu ./SS_NSS_fst_IDs.txt  --out ./SS_NSS
