73ec48c378a88ccdd6a3e9305fe219dba6e1bdf9
kj-lis <kj.lisiak@gmail.com>
2026-03-27 14:58:57 +0100

    Auto-sync 2026-03-27 14:58:57

--- scripts/hpc/14_fixation_index_filtr.sh
+++ scripts/hpc/14_fixation_index_filtr.sh
@@ -11,5 +11,5 @@
 #SBATCH --error=/home/jl430796/MaizePop/logs/zmays_14.%A_%a.err
 
 plink --bfile /home/jl430796/MaizePop/data/processed/00_SNP_odfiltr/chr_all_zea_plink_1 \
---keep /home/jl430796/MaizePop/metadane/05_fixation_index/SS_era1_era2_IDs.txt \
---make-bed --out /home/jl430796/MaizePop/data/processed/04_fixation_index/SS_era1_era2
+--keep /home/jl430796/MaizePop/metadane/05_fixation_index/SS_NSS_IDs.txt \
+--make-bed --out /home/jl430796/MaizePop/data/processed/04_fixation_index/SS_NSS
