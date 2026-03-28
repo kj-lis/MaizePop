0a60741c56af8a282e2bd306ef416356263f0836
kj-lis <kj.lisiak@gmail.com>
2026-03-28 16:54:48 +0100

    Auto-sync 2026-03-28 16:54:48

--- scripts/RStudio/16_fixation_index_analiza.R
+++ scripts/RStudio/16_fixation_index_analiza.R
@@ -22,35 +22,48 @@ summary(Tr_SS_1_clean$Fst)
 summary(Idt_1vs2_clean$Fst)
 summary(SS_1vs2_clean$Fst)
 
+
+
 fst_all <- rbind(
-  data.frame(Group = "Parviglumis vs Tropical", Fst = Pv_Tr_clean$Fst),
-  data.frame(Group = "Tropical vs Iodent era I", Fst = Tr_Idt_1_clean$Fst),
-  data.frame(Group = "Tropical vs SS era I", Fst = Tr_SS_1_clean$Fst),
-  data.frame(Group = "Iodent era I vs era II", Fst = Idt_1vs2_clean$Fst),
-  data.frame(Group = "SS era I vs era II", Fst = SS_1vs2_clean$Fst)
+  data.frame(Group = "Parviglumis vs. Tropical", Fst = Pv_Tr_clean$Fst),
+  data.frame(Group = "Tropical vs. Iodent era I", Fst = Tr_Idt_1_clean$Fst),
+  data.frame(Group = "Tropical vs. SS era I", Fst = Tr_SS_1_clean$Fst),
+  data.frame(Group = "Iodent era I vs. era II", Fst = Idt_1vs2_clean$Fst),
+  data.frame(Group = "SS era I vs. era II", Fst = SS_1vs2_clean$Fst)
 )
 
 fst_all$Group <- factor(
   fst_all$Group,
-  levels = c("Parviglumis vs Tropical", "Tropical vs Iodent era I", "Tropical vs SS era I",
-             "Iodent era I vs era II", "SS era I vs era II")
+  levels = c("Parviglumis vs. Tropical", "Tropical vs. Iodent era I", "Tropical vs. SS era I",
+             "Iodent era I vs. era II", "SS era I vs. era II")
 )
 
 library(ggplot2)
 
+png(file="C:/Users/kjlis/Desktop/fst_all.png", width=1000, height=950, res=150)
 ggplot(fst_all, aes(x = Group, y = Fst)) +
-  geom_boxplot(fill = "lightblue") +
+  geom_boxplot(fill = "aquamarine3") +
   theme_classic() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  labs(title = "fst between groups",
-       x = "Group",
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 12),
+        axis.title.x = element_text(size = 14, margin = margin(t = 10)),
+        axis.title.y = element_text(size = 14, margin = margin(r = 10)),
+        axis.text.y = element_text(size = 12),
+        plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
+        plot.margin = margin(10, 10, 10, 10)) +
+  labs(title = "fixation index between subpopulations",
+       x = "subpopulation",
        y = "fst")
+dev.off()
+
+
 
 Pv_Tr_chr <- split(Pv_Tr_clean, Pv_Tr_clean$Chr)
 Tr_Idt_1_chr <- split(Tr_Idt_1_clean, Tr_Idt_1_clean$Chr)
 Tr_SS_1_chr <- split(Tr_SS_1_clean, Tr_SS_1_clean$Chr)
 Idt_1vs2_chr <- split(Idt_1vs2_clean, Idt_1vs2_clean$Chr)
 SS_1vs2_chr <- split(SS_1vs2_clean, SS_1vs2_clean$Chr)
+
+library(GenWin)
 
 
 
@@ -59,32 +72,6 @@ SS_1vs2_chr <- split(SS_1vs2_clean, SS_1vs2_clean$Chr)
 
 #rbind, boxplot, SS I vs. SS II, Iodent stary vs. Iodent nowy
 #Tropical vs stary SS, tak samo Iodent
-
-PT$Fst <- pmax(PT$Fst, 0)
-TI$Fst <- pmax(TI$Fst, 0)
-TSS$Fst <- pmax(TSS$Fst, 0)
-TNS$Fst <- pmax(TNS$Fst, 0)
-
-PT_clean <- PT[!is.na(PT$Fst), ]
-TI_clean <- TI[!is.na(TI$Fst), ]
-TSS_clean <- TSS[!is.na(TSS$Fst), ]
-TNS_clean <- TNS[!is.na(TNS$Fst), ]
-
-summary(PT_clean$Fst)
-summary(TI_clean$FST)
-summary(TSS_clean$FST)
-summary(TNS_clean$FST)
-
-library(ggplot2)
-
-ggplot(PT_clean, aes(x = Fst)) +
-  geom_histogram(bins = 50, fill = "steelblue", color = "black") +
-  coord_cartesian(ylim = c(0, 4000000)) +
-  theme_minimal() + 
-  theme(panel.grid = element_blank()) +
-  labs(
-    x = "fixation index",
-    y = "number of SNPs")
 
 PT_chr1 <- PT_clean[PT_clean$Chr == 1, ]
 PT_chr2 <- PT_clean[PT_clean$Chr == 2, ]
@@ -104,97 +91,3 @@ PT_chr4_spline <- splineAnalyze(Y=chr4_subset$Fst,map=chr4_subset$bp,smoothness=
 
 
 
-
-################################
-
-
-
-
-fst_data <- read.table("fst_results.txt", header=TRUE) 
-head(fst_data)
-
-mean_fst <- mean(fst_data$FST, na.rm = TRUE)
-mean_fst
-
-hist(fst_data$FST,
-     breaks = 50,
-     col = "skyblue",
-     main = "FST: Parviglumis vs. Iodent",
-     xlab = "FST",
-     ylab = "Number of SNPs")
-
-abline(v = mean_fst, col = "red", lwd = 2)
-text(mean_fst, par("usr")[4]*0.9, labels = paste0("Mean = ", round(mean_fst,3)),
-     col="red", pos=4)
-
-boxplot(PT_clean$Fst,
-        horizontal = TRUE,
-        col = "lightgreen",
-        main = "FST: Parviglumis vs. Iodent",
-        xlab = "FST")
-
-points(mean_fst, 1, col = "red", pch = 19)
-
-
-
-
-library(dplyr)
-fst_by_chr <- TI_fst %>%
-  group_by(Chr) %>%
-  summarise(mean_fst = mean(Fst, na.rm=TRUE))
-
-barplot(fst_by_chr$mean_fst,
-        names.arg = fst_by_chr$Chr,
-        col = "steelblue",
-        xlab = "Chromosome",
-        ylab = "Mean FST",
-        main = "Mean FST per chromosome")
-mean_fst <- mean(TI_fst_clean$Fst, na.rm = TRUE)
-
-plot(density(PT_clean$Fst),
-     main="Density of FST",
-     xlab="FST",
-     ylab="Density",
-     col="blue", lwd=2)
-abline(v = mean_fst, col="red", lwd=2)
-
-
-
-
-
-
-fst1<- read.table("/home/kuba/Desktop/Parviglumis_Tropical.fst", header=TRUE)
-fst2<- read.table("/home/kuba/Desktop/Tropical_Iodent.fst", header=TRUE)
-fst3<- read.table("/home/kuba/Desktop/Tropical_SS.fst", header=TRUE)
-fst4<- read.table("/home/kuba/Desktop/Tropical_NSS.fst", header=TRUE)
-
-head(fst2)
-str(fst2)
-
-hist(PT_fst$Fst,
-     breaks=100,
-     col="steelblue",
-     main="Distribution of FST",
-     xlab="FST")
-
-install.packages("qqman")
-library(qqman)
-
-fst1_2 <- fst1
-fst1_2 <- fst1_2[!is.na(fst1_2$Fst), ]
-fst1_2$Fst[fst1_2$Fst < 0] <- 0
-
-manhattan(fst1_2,
-          chr="Chr",
-          bp="bp",
-          snp="SNP",
-          p="Fst",
-          logp=FALSE,
-          col=c("darkblue","orange"),
-          ylab="FST")
-
-
-PT_fst_2 <- PT_fst[PT_fst$Fst >= 0, ]
-TI_fst_2 <- TI_fst[TI_fst$Fst >= 0, ]
-TSS_fst_2 <- TSS_fst[TSS_fst$Fst >= 0, ]
-TNS_fst_2 <- TNS_fst[TNS_fst$Fst >= 0, ]
