9f486fc69f37f55869393d1ec04a139556a59eb1
kj-lis <kj.lisiak@gmail.com>
2026-04-01 14:12:28 +0200

    Auto-sync 2026-04-01 14:12:28

--- scripts/hpc/17_XP-EHH_filtr.sh
+++ scripts/hpc/17_XP-EHH_filtr.sh
@@ -10,3 +10,8 @@
 #SBATCH --output=/home/jl430796/MaizePop/logs/zmays_17.%A_%a.out
 #SBATCH --error=/home/jl430796/MaizePop/logs/zmays_17.%A_%a.err
 
+chr=${SLURM_ARRAY_TASK_ID}
+
+bcftools view -S /home/jl430796/MaizePop/metadane/03_XP_EHH/Tropical.txt \
+/home/jl430796/MaizePop/data/raw/chr_${chr}_zea_filtr.vcf.gz \
+-O z -o /home/jl430796/MaizePop/data/processed/05_XP_EHH/chr_${chr}_Tropical.vcf.gz
