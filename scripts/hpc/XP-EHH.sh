adf53da97b8658669a1a3120fb6f494fa50f4ea0
kj-lis <kj.lisiak@gmail.com>
2026-04-02 13:21:03 +0200

    Auto-sync 2026-04-02 13:21:03

--- scripts/hpc/XP-EHH.sh
+++ scripts/hpc/XP-EHH.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --job-name=zmays_18
+#SBATCH --job-name=xp_ehh
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
@@ -7,5 +7,6 @@
 #SBATCH --time=48:00:00
 #SBATCH --account=g102-2488
 #SBATCH --partition=okeanos
-#SBATCH --output=/home/jl430796/MaizePop/logs/zmays_18.%A_%a.out
-#SBATCH --error=/home/jl430796/MaizePop/logs/zmays_18.%A_%a.err
+#SBATCH --output=/home/jl430796/MaizePop/logs/xp_ehh.%A_%a.out
+#SBATCH --error=/home/jl430796/MaizePop/logs/xp_ehh.%A_%a.err
+#SBATCH --array=1-10
