b6e47b9d8fd72d89a61c80093a4d7fb2d9a8e33b
kj-lis <kj.lisiak@gmail.com>
2026-04-02 14:30:26 +0200

    Auto-sync 2026-04-02 14:30:26

--- scripts/hpc/XP-EHH_filtr_1.sh
+++ scripts/hpc/XP-EHH_filtr_1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --job-name=xp_ehh_1
+#SBATCH --job-name=zmays_17
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
@@ -7,13 +7,17 @@
 #SBATCH --time=48:00:00
 #SBATCH --account=g102-2488
 #SBATCH --partition=okeanos
-#SBATCH --output=/home/jl430796/MaizePop/logs/xp_ehh_1.%A_%a.out
-#SBATCH --error=/home/jl430796/MaizePop/logs/xp_ehh_1.%A_%a.err
+#SBATCH --output=/home/jl430796/MaizePop/logs/zmays_17.%A_%a.out
+#SBATCH --error=/home/jl430796/MaizePop/logs/zmays_17.%A_%a.err
 #SBATCH --array=1-10
 
 chr=${SLURM_ARRAY_TASK_ID}
+populations=("Parviglumis" "Tropical" "Iodent_1" "Iodent_2" "SS_1" "SS_2")
 
-java -Xmx45g -jar /home/jl430796/software/beagle.27Feb25.75f.jar \
-gt=/home/jl430796/MaizePop/data/raw/chr_${chr}_zea.vcf.gz \
-out=/home/jl430796/MaizePop/data/raw/inputed_chr_${chr} \
-nthreads=12
+for pop in "${populations[@]}"; do
+    bcftools view \
+        -S /home/jl430796/MaizePop/metadane/03_XP_EHH/${pop}.txt \
+        /home/jl430796/MaizePop/data/raw/chr_${chr}_zea_filtr.vcf.gz \
+        -O z -o /home/jl430796/MaizePop/data/processed/05_XP_EHH/chr_${chr}_${pop}.vcf.gz
+
+done
