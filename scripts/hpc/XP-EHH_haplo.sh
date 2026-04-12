73a8d0a0a1d7692b0695cc5da90ad34a5ffb62cb
kj-lis <kj.lisiak@gmail.com>
2026-04-12 20:24:31 +0200

    Auto-sync 2026-04-12 20:24:31

--- scripts/hpc/XP-EHH_haplo.sh
+++ scripts/hpc/XP-EHH_haplo.sh
@@ -11,6 +11,8 @@
 #SBATCH --error=/home/jl430796/MaizePop/logs/XP_EHH_haplo.%A_%a.err
 #SBATCH --array=1-10
 
+chr=${SLURM_ARRAY_TASK_ID}
+
 bcftools query -f '%CHROM\t%POS[\t%GT]\n' \
 /home/jl430796/MaizePop/data/processed/04_XP_EHH/XP_EHH_1/chr${chr}_Parviglumis.vcf.gz \
 | awk '{
