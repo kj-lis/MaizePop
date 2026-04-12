94e8ea977ea0fbe229ceae766f15d2c1a8916279
kj-lis <kj.lisiak@gmail.com>
2026-04-12 20:59:21 +0200

    Auto-sync 2026-04-12 20:59:21

--- scripts/hpc/XP-EHH.sh
+++ scripts/hpc/XP-EHH.sh
@@ -16,6 +16,6 @@ chr=${SLURM_ARRAY_TASK_ID}
 /home/jl430796/software/selscan/bin/linux/selscan \
 --xpehh \
 --hap /home/jl430796/MaizePop/data/processed/04_XP_EHH/XP_EHH_2/chr${chr}_Parviglumis.hap \
---ref /home/jl430796/MaizePop/data/processed/04_XP_EHH/XP_EHH_2/${chr}_Tropical.hap \
---pmap /home/jl430796/MaizePop/data/processed/04_XP_EHH/XP_EHH_2/chr${chr}.map \
---out /home/jl430796/MaizePop/results/full/04_XP_EHHchr${chr}_Pv_Tr \
+--ref /home/jl430796/MaizePop/data/processed/04_XP_EHH/XP_EHH_2/chr${chr}_Tropical.hap \
+--pmap \
+--out /home/jl430796/MaizePop/results/full/04_XP_EHH/chr${chr}_Pv_Tr \
