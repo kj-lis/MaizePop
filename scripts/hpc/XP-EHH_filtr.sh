7fef6b5e7d553e871663540f091b07133d1d0e0e
kj-lis <kj.lisiak@gmail.com>
2026-04-12 19:47:54 +0200

    Auto-sync 2026-04-12 19:47:54

--- scripts/hpc/XP-EHH_filtr.sh
+++ scripts/hpc/XP-EHH_filtr.sh
@@ -1,11 +1,9 @@
-#do poprawienia
-
 #!/bin/bash
 #SBATCH --job-name=XP_EHH_filtr
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=5G
+#SBATCH --mem=10G
 #SBATCH --time=48:00:00
 #SBATCH --account=g102-2488
 #SBATCH --partition=okeanos
@@ -13,13 +11,15 @@
 #SBATCH --error=/home/jl430796/MaizePop/logs/XP_EHH_filtr.%A_%a.err
 #SBATCH --array=1-10
 
+set -euo pipefail
+
 chr=${SLURM_ARRAY_TASK_ID}
 populations=("Parviglumis" "Tropical" "Iodent_1" "Iodent_2" "SS_1" "SS_2")
 
 for pop in "${populations[@]}"; do
     bcftools view \
         -S /home/jl430796/MaizePop/metadane/04_XP_EHH/${pop}.txt \
-        /home/jl430796/MaizePop/data/raw/inputed/chr${chr}_inputed.vcf.gz \
+        /home/jl430796/MaizePop/data/raw/imputed/chr${chr}_imputed.vcf.gz \
         -O z -o /home/jl430796/MaizePop/data/processed/04_XP_EHH/chr${chr}_${pop}.vcf.gz
 done
 
