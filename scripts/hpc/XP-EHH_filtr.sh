9257be28b1bcdf98e65b39166b80afb140086872
kj-lis <kj.lisiak@gmail.com>
2026-04-12 19:51:40 +0200

    Auto-sync 2026-04-12 19:51:40

--- scripts/hpc/XP-EHH_filtr.sh
+++ scripts/hpc/XP-EHH_filtr.sh
@@ -19,7 +19,7 @@ populations=("Parviglumis" "Tropical" "Iodent_1" "Iodent_2" "SS_1" "SS_2")
 for pop in "${populations[@]}"; do
     bcftools view \
         -S /home/jl430796/MaizePop/metadane/04_XP_EHH/${pop}.txt \
-        /home/jl430796/MaizePop/data/raw/imputed/chr${chr}_imputed.vcf.gz \
+        /home/jl430796/MaizePop/data/raw/imputed/chr${chr}_fixed.vcf.gz \
         -O z -o /home/jl430796/MaizePop/data/processed/04_XP_EHH/chr${chr}_${pop}.vcf.gz
 done
 
