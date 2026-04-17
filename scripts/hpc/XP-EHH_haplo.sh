d64f6e3647ea1271791d3f7ec8e2aa966779fe80
kj-lis <kj.lisiak@gmail.com>
2026-04-17 09:46:52 +0200

    Auto-sync 2026-04-17 09:46:52

--- scripts/hpc/XP-EHH_haplo.sh
+++ scripts/hpc/XP-EHH_haplo.sh
@@ -14,7 +14,7 @@
 chr=${SLURM_ARRAY_TASK_ID}
 
 bcftools query -f '[%GT\t]\n' \
-/home/jl430796/MaizePop/data/processed/04_XP_EHH/XP_EHH_1/chr${chr}_Iodent_1.vcf.gz \
+/home/jl430796/MaizePop/data/processed/04_XP_EHH/XP_EHH_1/chr${chr}_SS_1.vcf.gz \
 | awk '
 {
     for(i=1; i<=NF; i++) {
@@ -28,7 +28,7 @@ END {
         print substr(h1[i],2);
         print substr(h2[i],2);
     }
-}' > /home/jl430796/MaizePop/data/processed/04_XP_EHH/XP_EHH_2/chr${chr}_Iodent_1.hap
+}' > /home/jl430796/MaizePop/data/processed/04_XP_EHH/XP_EHH_2/chr${chr}_SS_1.hap
 
 MAKE_MAP=false
 
