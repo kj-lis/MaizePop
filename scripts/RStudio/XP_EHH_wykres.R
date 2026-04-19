6951498eb8b22f73af3a64e89e1fb040576e3b1f
kj-lis <kj.lisiak@gmail.com>
2026-04-19 20:00:02 +0200

    Auto-sync 2026-04-19 20:00:02

--- scripts/RStudio/XP_EHH_wykres.R
+++ scripts/RStudio/XP_EHH_wykres.R
@@ -1,1 +1,2 @@
-/home/jl430796/software/selscan/bin/linux/selscan norm --xpehh --files
+/home/jl430796/software/selscan/bin/linux/selscan 
+norm -- xpehh -- files chr1.xpehh.out chr2.xpehh.out --bp-win
