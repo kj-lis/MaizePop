d91b1d63087423a1bf46b3f79f18e83c245300ea
kj-lis <kj.lisiak@gmail.com>
2026-04-21 10:24:14 +0200

    Auto-sync 2026-04-21 10:24:14

--- scripts/hpc/FST.sh
+++ scripts/hpc/FST.sh
@@ -1,24 +1,22 @@
-/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/Parviglumis_Tropical_FST --fst --sub-popu ./Parviglumis_Tropical_fst_IDs.txt  --out ./Parviglumis_Tropical
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/Parviglumis_Tropical_FST --fst --sub-popu ./Parviglumis_Tropical_FST_IDs.txt  --out ./Parviglumis_Tropical
 
-/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/Tropical_Iodent_1_FST --fst --sub-popu ./Tropical_Iodent_1_fst_IDs.txt  --out ./Tropical_Iodent_1
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/Tropical_Iodent_1_FST --fst --sub-popu ./Tropical_Iodent_1_FST_IDs.txt  --out ./Tropical_Iodent_1
 
-/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/Tropical_SS_1_FST --fst --sub-popu ./Tropical_SS_1_fst_IDs.txt  --out ./Tropical_SS_1
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/Tropical_SS_1_FST --fst --sub-popu ./Tropical_SS_1_FST_IDs.txt  --out ./Tropical_SS_1
 
-/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/Iodent_1vs2_FST --fst --sub-popu ./Iodent_1vs2_fst_IDs.txt  --out ./Iodent_1vs2
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/Iodent_1vs2_FST --fst --sub-popu ./Iodent_1vs2_FST_IDs.txt  --out ./Iodent_1vs2
 
-/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/SS_1vs2_FST --fst --sub-popu ./SS_1vs2_fst_IDs.txt  --out ./SS_1vs2
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/SS_1vs2_FST --fst --sub-popu ./SS_1vs2_FST_IDs.txt  --out ./SS_1vs2
 
-/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/Iodent_1_SS_1_FST --fst --sub-popu ./Iodent_1_SS_1_fst_IDs.txt  --out ./Iodent_1_SS_1
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/Iodent_1_SS_1_FST --fst --sub-popu ./Iodent_1_SS_1_FST_IDs.txt  --out ./Iodent_1_SS_1
 
-/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/Iodent_2_SS_2_FST --fst --sub-popu ./Iodent_2_SS_2_fst_IDs.txt  --out ./Iodent_2_SS_2
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/Iodent_2_SS_2_FST --fst --sub-popu ./Iodent_2_SS_2_FST_IDs.txt  --out ./Iodent_2_SS_2
 
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/Iodent_1_NSS_FST --fst --sub-popu ./Iodent_1_NSS_FST_IDs.txt  --out ./Iodent_1_NSS
 
-################################
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/Iodent_2_NSS_FST --fst --sub-popu ./Iodent_2_NSS_FST_IDs.txt  --out ./Iodent_2_NSS
 
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/SS_1_NSS_FST --fst --sub-popu ./SS_1_NSS_FST_IDs.txt  --out ./SS_1_NSS
 
-/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/Mexicana_Parviglumis_FST --fst --sub-popu ./Mexicana_Parviglumis_fst_IDs.txt  --out ./Mexicana_Parviglumis
-
-/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/Tropical_NSS_FST --fst --sub-popu ./Tropical_NSS_fst_IDs.txt  --out ./Tropical_NSS
-
-/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/SS_NSS_FST --fst --sub-popu ./SS_NSS_fst_IDs.txt  --out ./SS_NSS
+/home/kuba/Desktop/gcta-1.95.1-linux-x86_64./gcta-1.95.1-linux-x86_64/gcta64 --bfile ./03_FST/SS_2_NSS_FST --fst --sub-popu ./SS_2_NSS_FST_IDs.txt  --out ./SS_2_NSS
 
