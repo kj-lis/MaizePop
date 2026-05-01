e9838337d51add094c61a8007250ef0b48bb7f9d
kj-lis <kj.lisiak@gmail.com>
2026-05-01 22:49:33 +0200

    Auto-sync 2026-05-01 22:49:33

--- scripts/RStudio/admix_analiza.R
+++ scripts/RStudio/admix_analiza.R
@@ -1,26 +1,20 @@
-Q <- read.table("/home/kuba/Desktop/zmays/admix/chr_all_zmays_plink_3.7.Q")
-metadane <- read.csv("/home/kuba/Desktop/zmays_uniq.csv", stringsAsFactors = FALSE, sep = ";")
+Q <- read.table("C:/Users/kjlis/Desktop/chr_all_admix_2.9.Q")
+fam <- read.table("C:/Users/kjlis/Desktop/chr_all_admix_2.fam")
+metadane <- read.csv("C:/Users/kjlis/Desktop/metadane_all.csv", stringsAsFactors = FALSE, sep = ";")
 
-colnames(Q) <- paste0("K",1:7)
-data <- cbind(metadane$VCFname, Q)
-colnames(data)[1] <- "ID"
+colnames(fam)[2] <- "ID"
 
-all(metadane$VCFname == data$ID)
+metadane_ordered <- metadane[match(fam$ID, metadane$VCFname), ]
+all(fam$ID == metadane_ordered$VCFname)
 
-data$Assigned_cluster <- apply(Q, 1, which.max)
-merged <- merge(data, metadane, by.x="ID", by.y="VCFname")
+colnames(Q) <- paste0("K",1:9)
+admix <- cbind(metadane_ordered, Q)
 
-write.csv(merged, "/home/kuba/Desktop/zmays_admix7.csv", row.names=FALSE)
-table(merged$Assigned_cluster)
-table(merged$Assigned_cluster, merged$Q)
+idx <- which(names(admix) == "subpopulation")
+admix <- cbind(
+  admix[, 1:idx],
+  assigned_cluster = max.col(Q),
+  admix[, (idx+1):ncol(admix)])
 
-polish_lines <- merged[merged$origin == "Poland", ]
-write.csv(polish_lines, "/home/kuba/Desktop/zmays_admix7_pl.csv", row.names=FALSE)
-table(polish_lines$Assigned_cluster)
+write.csv(admix, "C:/Users/kjlis/Desktop/admix_K9.csv", row.names=FALSE)
 
-K <- max(merged$Assigned_cluster)
-
-for(i in 1:K){
-  cluster_ids <- merged$ID[merged$Assigned_cluster == i]
-  writeLines(cluster_ids, paste0("/home/kuba/Desktop/zea", i, "_IDs.txt"))
-}
