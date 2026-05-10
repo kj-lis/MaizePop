e198f8a2c030a2a0eafef99e5dc3f7fb9225f082
kj-lis <kj.lisiak@gmail.com>
2026-05-10 20:26:54 +0200

    Auto-sync 2026-05-10 20:26:54

---
+++ scripts/hpc/XP-EHH_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=XP_EHH
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10G
+#SBATCH --time=48:00:00
+#SBATCH --account=g102-2488
+#SBATCH --partition=okeanos
+#SBATCH --output=/home/jl430796/MaizePop/logs/test.%A_%a.out
+#SBATCH --error=/home/jl430796/MaizePop/logs/test.%A_%a.err
+
+/home/jl430796/software/selscan/bin/linux/selscan \
+  --xpehh \
+  --vcf /home/jl430796/MaizePop/data/processed/04_XP_EHH/1_XP_EHH/chr1_Tropical.vcf.gz \
+  --vcf-ref /home/jl430796/MaizePop/data/processed/04_XP_EHH/1_XP_EHH/chr1_Parviglumis.vcf.gz \
+  --out /home/jl430796/MaizePop/data/processed/04_XP_EHH/2_XP_EHH/chr1_Trop_vs_Parv
