f6e5a91aa7b09fc201882b9e85b7e91be5bf0f3c
kj-lis <kj.lisiak@gmail.com>
2026-05-13 22:47:38 +0200

    Auto-sync 2026-05-13 22:47:37

--- scripts/hpc/XP-EHH_norm.sh
+++ scripts/hpc/XP-EHH_norm.sh
@@ -1,1 +1,3 @@
 /home/jl430796/software/selscan/bin/linux/selscan norm --xpehh --files chr1_Pv_Tr.xpehh.out chr2_Pv_Tr.xpehh.out chr3_Pv_Tr.xpehh.out chr4_Pv_Tr.xpehh.out chr5_Pv_Tr.xpehh.out chr6_Pv_Tr.xpehh.out chr7_Pv_Tr.xpehh.out chr8_Pv_Tr.xpehh.out chr9_Pv_Tr.xpehh.out chr10_Pv_Tr.xpehh.out
+
+/home/jl430796/software/selscan/bin/linux/selscan norm --xpehh --files chr1_Tr_Idt_1.xpehh.out chr2_Tr_Idt_1.xpehh.out chr3_Tr_Idt_1.xpehh.out chr4_Tr_Idt_1.xpehh.out chr5_Tr_Idt_1.xpehh.out chr6_Tr_Idt_1.xpehh.out chr7_Tr_Idt_1.xpehh.out chr8_Tr_Idt_1.xpehh.out chr9_Tr_Idt_1.xpehh.out chr10_Tr_Idt_1.xpehh.out
