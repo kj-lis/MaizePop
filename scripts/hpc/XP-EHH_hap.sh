5c0f3e4d813ce5bcff306fdc7d5c517401a3738c
kj-lis <kj.lisiak@gmail.com>
2026-05-17 12:23:40 +0200

    Auto-sync 2026-05-17 12:23:40

--- scripts/hpc/XP-EHH_hap.sh
+++ scripts/hpc/XP-EHH_hap.sh
@@ -1,4 +1,78 @@
 #!/bin/bash
+#SBATCH --job-name=XP_EHH_hap
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10G
+#SBATCH --time=48:00:00
+#SBATCH --account=g102-2488
+#SBATCH --partition=okeanos
+#SBATCH --output=/home/jl430796/MaizePop/logs/XP_EHH_hap.%A_%a.out
+#SBATCH --error=/home/jl430796/MaizePop/logs/XP_EHH_hap.%A_%a.err
+#SBATCH --array=1-10
+
+chr=${SLURM_ARRAY_TASK_ID}
+
+INPUT_DIR=/home/jl430796/MaizePop/data/processed/04_XP_EHH/1_XP_EHH
+OUTPUT_DIR=/home/jl430796/MaizePop/data/processed/04_XP_EHH/3_XP_EHH
+
+bcftools query -f '[%GT\t]\n' chr1_Tropical.vcf.gz | \
+awk '
+{
+    for(i=1; i<=NF; i++) {
+        split($i,a,"|");
+        h1[i]=h1[i]" "a[1];
+        h2[i]=h2[i]" "a[2];
+    }
+}
+END {
+    for(i=1; i<=NF; i++) {
+        print substr(h1[i],2);
+        print substr(h2[i],2);
+    }
+}' > chr1_Tropical.hap
+
+
+
+
+
+
+
+
+
+
+
+
+bcftools query -f '[%GT\t]\n' chr1_Tropical.vcf.gz | \
+awk '
+{
+    for(i=1; i<=NF; i++) {
+        split($i,a,"|");
+        h1[i]=h1[i]" "a[1];
+        h2[i]=h2[i]" "a[2];
+    }
+}
+END {
+    for(i=1; i<=NF; i++) {
+        print substr(h1[i],2);
+        print substr(h2[i],2);
+    }
+}' > chr1_Tropical.hap
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#!/bin/bash
 #SBATCH --job-name=XP_EHH_haplo
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
