a93d2c5264725b17de0166502763e072c17eb83e
kj-lis <kj.lisiak@gmail.com>
2026-05-21 14:38:21 +0200

    Auto-sync 2026-05-21 14:38:21

--- scripts/hpc/XP-CLR.sh
+++ scripts/hpc/XP-CLR.sh
@@ -7,3 +7,6 @@ for i in {1..10}; do /home/kuba/Desktop/xp./xp/XPCLR -xpclr /home/kuba/Desktop/0
 for i in {1..10}; do /home/kuba/Desktop/xp./xp/XPCLR -xpclr /home/kuba/Desktop/05_XP_CLR/chr${i}_Iodent_2.geno /home/kuba/Desktop/05_XP_CLR/chr${i}_Iodent_1.geno /home/kuba/Desktop/XP_CLR_mapa/chr${i}.snp /home/kuba/Desktop/XP_CLR_out/Idt_1vs2/chr${i}_Idt_1vs2 -w1 0.02 800 10000 ${i} -p0 0.95; done
 
 for i in {1..10}; do /home/kuba/Desktop/xp./xp/XPCLR -xpclr /home/kuba/Desktop/05_XP_CLR/chr${i}_SS_2.geno /home/kuba/Desktop/05_XP_CLR/chr${i}_SS_1.geno /home/kuba/Desktop/XP_CLR_mapa/chr${i}.snp /home/kuba/Desktop/XP_CLR_out/SS_1vs2/chr${i}_SS_1vs2 -w1 0.02 800 10000 ${i} -p0 0.95; done
+
+
+for i in {1..10}; do (/home/kuba/Desktop/xp/xp/XPCLR -xpclr /home/kuba/Desktop/05_XP_CLR/chr${i}_Tropical.geno /home/kuba/Desktop/05_XP_CLR/chr${i}_Parviglumis.geno /home/kuba/Desktop/XP_CLR_mapa/chr${i}.snp /home/kuba/Desktop/XP_CLR_out/Pv_Tr/chr${i}_Pv_Tr -w1 0.02 800 10000 ${i} -p0 0.95) & if (( $(jobs -r | wc -l) >= 4 )); then wait -n; fi; done; wait
